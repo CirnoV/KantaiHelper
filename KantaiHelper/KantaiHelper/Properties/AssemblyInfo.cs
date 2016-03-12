fbdaf49c1248b3f0929d6b54f984f2073ee2fa35
Cirno V <cirnov59@gmail.com>
2016-03-12 23:08:11 +0900

    기본 작업

--- KantaiHelper/KantaiHelper/Properties/AssemblyInfo.cs
+++ KantaiHelper/KantaiHelper/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
 [assembly: AssemblyTitle("KantaiHelper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("KanColleViewer에 함대 관리 기능을 추가합니다.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("KantaiHelper")]
@@ -51,5 +51,5 @@ using System.Windows;
 // 모든 값을 지정하거나 아래와 같이 '*'를 사용하여 빌드 번호 및 수정 번호가 자동으로 
 // 지정되도록 할 수 있습니다.
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
--- KantaiHelper/KantaiHelper/UserControl1.xaml.cs
+++ KantaiHelper/KantaiHelper/UserControl1.xaml.cs
@@ -15,14 +15,14 @@ using System.Windows.Shapes;
 
 namespace KantaiHelper
 {
-    /// <summary>
-    /// UserControl1.xaml에 대한 상호 작용 논리
-    /// </summary>
-    public partial class UserControl1 : UserControl
-    {
-        public UserControl1()
-        {
-            InitializeComponent();
-        }
-    }
+	/// <summary>
+	/// UserControl1.xaml에 대한 상호 작용 논리
+	/// </summary>
+	public partial class UserControl1 : UserControl
+	{
+		public UserControl1()
+		{
+			InitializeComponent();
+		}
+	}
 }
