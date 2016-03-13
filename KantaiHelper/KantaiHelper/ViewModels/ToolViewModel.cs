89095efc70782a2139f465c26726df44fbac7e15
Cirno V <cirnov59@gmail.com>
2016-03-13 23:38:07 +0900

    Expander랑 ScrollViewer디자인 적용. (BattleInfoPlugin에서 일부 Source를 차용했습니다.)

---
+++ KantaiHelper/KantaiHelper/ViewModels/ToolViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Livet;
+using Livet.Messaging;
+
+namespace KantaiHelper.ViewModels
+{
+	class ToolViewModel : ViewModel
+	{
+		
+	}
+}
